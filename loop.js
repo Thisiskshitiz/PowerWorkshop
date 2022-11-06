1ccc73983916a6c88308b502dbe3ce3e353bf700
Kshitiz Shrestha <thisiskshitizz@gmail.com>
2022-11-06 11:52:47 +0545

    Initial Commit

---
+++ loop.js
@@ -0,0 +1,35 @@
+// function myApp() {
+//   const arr = [2, 5, 6, 7, 8, 9, 10];
+//   var mapArr;
+//   var filterArr;
+//   //   console.log(typeof mapArr);
+//   //   arr.forEach(foreach1);
+//   //   arr.forEach(foreach2);
+//   //   console.log(arr);
+//   mapArr = arr.map(map);
+//   console.log(mapArr);
+//   console.log(arr);
+//   console.log(arr.filter(check));
+// }
+
+// function foreach1(num) {
+//   console.log(num);
+// }
+
+// function foreach2(item, index, arr) {
+//   arr[index] = item * 10;
+// }
+//map
+// function check(num) {
+//   return num > 5;
+// }
+
+// function map(items) {
+//   items = `${items} hello`;
+//   return items;
+// }
+// myApp();
+setTimeout(() => {
+  console.log("Test Statement 1");
+}, 2000);
+console.log("Test Statement 2");
